cd03ca8dbaaf5e3ff7b1800e976bfe54ed2fc1b5
Himanshu Kakkar <himanshukakkar917@gmail.com>
2025-04-03 16:08:45 +0530

    exploring

--- src/app.js
+++ src/app.js
@@ -3,25 +3,45 @@ const express = require("express");
 const app = express();
 const PORT = 7777;
 
-const authMiddleware = ((req, res, next)=>  {
-    console.log("Auth middleware ran.")
-    next();
-})
+
+// after /hello handle by this
+// http://localhost:7777/hello/123 
+// not http://localhost:7777/hello123 NOT
 
 app.use("/hello", (req,res) => {
     res.send("Hello World");
 });
-
-app.get("/test", authMiddleware,(req,res) => {
+ 
+// after /test handle by this
+app.get("/test",(req,res) => {
     res.send("Testing server");
 });
 
-// app.use("/", (req,res,next) => {
+app.use("/user", (req,res) => {
+    res.send("HAHAHAAAAAHHAHAHA");
+})
 
-//     next();
-//     res.send("Auth has been checked");
-// });
 
+app.get("/user", (req,res) => {
+    res.send({firstName: "Akshay", lastname:"Saini"});
+})
+
+
+app.post("/user", (req,res) => {
+    // saving data on DB
+    res.send("Data saved successfully to the DB");
+})
+
+app.delete("/user", (req,res) => {
+    res.send("data deleted successfully");
+})
+
+// after / anything handle by this 
+// wild card route 
+// if anything doesnt match with route afer / this will work
+// app.use("/", (req,res) => {
+//     res.send("Namaste NodeJS")
+// })
 
 app.listen(PORT, () => {
     console.log("server is started at port:",PORT);
@@ -35,3 +55,17 @@ app.listen(PORT, () => {
 
 // For example, in a Node.js + Express project, Express itself is a 
 // dependency because your app needs it to run.
+
+// ORDER OF CODE DOES MATTER A LOT
+
+// const authMiddleware = ((req, res, next)=>  {
+//     console.log("Auth middleware ran.")
+//     next();
+// })
+
+
+// app.use("/", (req,res,next) => {
+
+//     next();
+//     res.send("Auth has been checked");
+// });
