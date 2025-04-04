f7649e867b8614b9df49809c42461b635a0e40cf
Himanshu Kakkar <himanshukakkar917@gmail.com>
2025-04-04 21:29:24 +0530

    episode_05

--- src/app.js
+++ src/app.js
@@ -2,6 +2,15 @@ const express = require("express");
 
 const app = express();
 
+app.use("/user", (req,res) => {
+
+    // res.send("route handle 1");
+})
+
+// app.use("/",(req,res) => {
+//     res.send("chal gya");
+// })
+
 
 app.listen(7777, ()=> {
     console.log("server is created successfully on port 7777" );
