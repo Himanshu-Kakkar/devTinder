222b5f237194a14d90115b478bd512fce0a11c65
Himanshu Kakkar <himanshukakkar917@gmail.com>
2025-04-15 13:01:57 +0530

    JSON vs JS object

---
+++ readme.md
@@ -0,0 +1,18 @@
+# S02 E07
+
+post query is used to send data to mongodb.
+to store data to database without changing in code just like actual project using POST
+
+## POSTMAN
+POST > body > raw > json
+xml was used widely before json
+
+#### difference between JS object and JSON ?
+    data is in key-value pair
+    In JSON key is also a string
+    JS object can take comma after last feild
+    while JSON can not
+    our server cant read JSON data
+    server can read JS object
+    
+
--- src/app.js
+++ src/app.js
@@ -4,26 +4,56 @@ const app = express();
 const connectDB = require("./config/database.js");
 const User = require("./models/user.js");
 
+// correct
+app.use(express.json()); // applied middleware to every route
+// Now we will get JS object and server can read and console.log() works
+//wrong 
+// bcz express.json() returns a middleware
+// app.use(() => {
+//     express.json();
+// });
 
 app.post("/signup", async (req,res)=> {
 
-    // creating a new instance of the user model
-    const user = new User({
-        firstName: "Virat",
-        lastName: "Kohli",
-        emailId: "vk@gmail.com",
-        password: "anushka",
-        // _id: "uniqueID" // dont mess up with this
-        // lets mongodb handle this
-        // dont interfare in the automatic feilds of 
-    });
+    console.log(req.body); // undefined
+    // bcz our server cant read JSON data
+    // server can read JS object
+    // need a middleware to convert JSON to JS object
+    // predefined middleware by express
+    // express.json()
+    // save the data using postman
+    const user = new User(req.body);
     try {
-        await user.save(); // returns a promise
-        // almost every mongodb function return a promise so always try to use async await
+        await user.save(); 
         res.send("user added successfully");
     } catch (err) {
         res.status(400).send("Error while saving the user", err.message);
     }
+    // mannually save the data from the code
+    // creating a new instance of the user model
+    // this is NOT JSON 
+    // this is JS object
+    // data is in key-value pair
+    // In JSON key is also a string
+    // JS object can take comma after last feild
+    // while JSON can not
+
+    // const user = new User({
+    //     firstName: "Virat",
+    //     lastName: "Kohli",
+    //     emailId: "vk@gmail.com", 
+    //     password: "anushka",
+    //     // _id: "uniqueID" // dont mess up with this
+    //     // lets mongodb handle this
+    //     // dont interfare in the automatic feilds of 
+    // });
+    // try {
+    //     await user.save(); // returns a promise
+    //     // almost every mongodb function return a promise so always try to use async await
+    //     res.send("user added successfully");
+    // } catch (err) {
+    //     res.status(400).send("Error while saving the user", err.message);
+    // }
     
     
 })
